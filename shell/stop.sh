48c77ff6215a2a51805fb5cb166712404262bcac
Andrew Balaban <balaban.andrew.htc@gmail.com>
2018-10-02 22:30:43 +0300

    #5 shell scripts

---
+++ shell/stop.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+docker container stop aether3
+docker container stop docker-aether3_phpmyadmin_1
+docker container stop docker-aether3_memcached_1
+docker container stop docker-aether3_db_1
+docker rm aether3
+docker rm docker-aether3_phpmyadmin_1
+docker rm docker-aether3_memcached_1
+docker rm docker-aether3_db_1
+docker rm docker-aether3_composer_1
