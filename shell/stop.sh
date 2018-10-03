50cbc39b757d4fd87f44e3e2e9e96a5494a75a53
Andrew Balaban <balaban.andrew.htc@gmail.com>
2018-10-03 16:22:53 +0300

    add phpmemcahcedadmin to compose

--- shell/stop.sh
+++ shell/stop.sh
@@ -3,8 +3,10 @@ docker container stop aether3
 docker container stop docker-aether3_phpmyadmin_1
 docker container stop docker-aether3_memcached_1
 docker container stop docker-aether3_db_1
+docker container stop docker-aether3_phpmemcachedadmin_1
 docker rm aether3
 docker rm docker-aether3_phpmyadmin_1
 docker rm docker-aether3_memcached_1
 docker rm docker-aether3_db_1
 docker rm docker-aether3_composer_1
+docker rm docker-aether3_phpmemcachedadmin_1
